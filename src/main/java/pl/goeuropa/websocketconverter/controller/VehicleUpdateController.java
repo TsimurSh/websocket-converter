b1a4356970605590037009aa98a9a462446b92c2
Timur Shykhsefiyeu <nut.t@icloud.com>
2024-03-19 15:20:04 +0100

    refactoring + clean up code: edit

--- src/main/java/pl/goeuropa/websocketconverter/controller/VehicleUpdateController.java
+++ src/main/java/pl/goeuropa/websocketconverter/controller/VehicleUpdateController.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/kombus")
 public class VehicleUpdateController {
 
     private final VehicleUpdateService service;
